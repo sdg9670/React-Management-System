d5ca66898d36d69aac70af162bb53346ba887c4b
sdg9670 <sdg9670@naver.com>
2019-11-03 11:26:52 +0900

    Create Customer Database Table & Node.js Database Connection Setting

--- server.js
+++ server.js
@@ -1,39 +1,26 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const fs = require('fs');
 const app = express();
 const port = process.env.PORT || 5000
-const customers = [
-    {
-      'id': 1,
-      'image': 'https://placeimg.com/64/64/1',
-      'name': '홍길동',
-      'birthday': '961222',
-      'gender': '남자',
-      'job': '대학생'
-    },
-    {
-      'id': 2,
-      'image': 'https://placeimg.com/64/64/2',
-      'name': '장준수',
-      'birthday': '970128',
-      'gender': '여자',
-      'job': '백수'
-    },
-    {
-      'id': 3,
-      'image': 'https://placeimg.com/64/64/3',
-      'name': '이재영',
-      'birthday': '961205',
-      'gender': '남자',
-      'job': '편입생'
-    }
-  ];
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+const data = fs.readFileSync('./database.json');
+const conf = JSON.parse(data);
+const mysql = require('mysql');
+const connection = mysql.createConnection({
+  host: conf.host,
+  user: conf.user,
+  password: conf.password,
+  port: conf.port,
+  database: conf.database
+});
+connection.connect();
+
 app.get('/api/customers', (req, res) => {
-    res.send(customers);
-})
+    connection.query("SELECT * FROM CUSTOMER", (err, rows, fields) => { res.send(rows); });
+});
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
